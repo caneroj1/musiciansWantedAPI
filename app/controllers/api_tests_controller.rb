6ecaaa163d9fd2a18925e8b9291c776e73ffd23c
Hank Harvey <harveyh1@tcnj.edu>
2015-03-04 16:57:11 -0500

    created test controller, removed readme.rdoc

---
+++ app/controllers/api_tests_controller.rb
@@ -0,0 +1,2 @@
+class ApiTestsController < ApplicationController
+end
---
+++ test/controllers/api_tests_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ApiTestsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
