b04b5696e7879d01828b95e8ae914ce5b8d2cd22
Hank Harvey <harveyh1@tcnj.edu>
2015-03-04 18:24:20 -0500

    made user mode

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,23 @@
 class UsersController < ApplicationController
+	def user_params
+      params.require(:user).permit(:name)
+    end
+
 	def index
+
+		@user_info = User.all()
 		respond_to do |format|
-		   format.html { }
-		   format.json  { render :json => "name": "John", "age": 45 }
+		   format.html
+		   format.json  { render :json => @user_info}
 		end
 	end
+
+	# def show
+
+	# 	@user_info = User.find(params[:id])
+	# 	respond_to do |format|
+	# 	   format.html
+	# 	   format.json  { render :json => @user_info}
+	# 	end
+	# end
 end
