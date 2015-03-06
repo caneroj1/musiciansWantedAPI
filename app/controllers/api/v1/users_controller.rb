aba2cdff29075dabfda190cd01e702ffb56ff4ae
caneroj1 <caneroj1@tcnj.edu>
2015-03-06 13:36:42 -0500

    added brief form to create new users.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,22 @@
 class Api::V1::UsersController < ApplicationController
 	def index
 		@user_info = User.all
-		
+
 		respond_to do |format|
 		   format.json  { render :json => @user_info}
+		end
+	end
+
+	def new
+		@new_user = User.new
+	end
+
+	def create
+		@new_user = User.new(params[:user])
+		if @new_user.save
+			redirect_to api_users_path
+		else
+			render "new.html"
 		end
 	end
 end
