6f22efe5f688d9e5690e5948597c05670f2ff775
Hank Harvey <harveyh1@tcnj.edu>
2015-03-06 14:42:50 -0500

    Version reading is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
-    scope module: :v1 do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, defaults: { format: :json }
     end
   end
