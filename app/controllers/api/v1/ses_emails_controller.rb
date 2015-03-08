bd2c104a04f3dd7aee2dd4f6fea4f06e6ab1ba7b
Hank Harvey <harveyh1@tcnj.edu>
2015-03-08 18:59:25 -0400

    SES Client working, returning pageable response data

--- app/controllers/api/v1/ses_emails_controller.rb
+++ app/controllers/api/v1/ses_emails_controller.rb
@@ -1,3 +1,14 @@
 class Api::V1::SesEmailsController < ApplicationController
-  
+  before_action :loadClient
+  def loadClient()
+    @sesClient = Aws::SES::Client.new(access_key_id: ENV['aws_access_key_id'],secret_access_key: ENV['aws_secret_access_key'],region: 'us-east-1')
+  end
+
+  def test()
+
+      @sesClient.get_send_quota.each do |response|
+        @resp = response.data.max_send_rate
+      end
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-  namespace :api, defaults: { format: :json } do
+  namespace :api do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, defaults: { format: :json }
+      get 'test', to: 'ses_emails#test'
     end
   end
 end
