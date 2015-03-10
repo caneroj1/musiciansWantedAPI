a5cfbda17cbb6c5ac54ce940ac909bd358fe2bfd
Hank Harvey <harveyh1@tcnj.edu>
2015-03-10 15:16:26 -0400

    Verify new emails, undertand SES better

--- app/controllers/api/v1/ses_emails_controller.rb
+++ app/controllers/api/v1/ses_emails_controller.rb
@@ -2,13 +2,24 @@ class Api::V1::SesEmailsController < ApplicationController
   before_action :loadClient
   def loadClient()
     @sesClient = Aws::SES::Client.new(access_key_id: ENV['aws_access_key_id'],secret_access_key: ENV['aws_secret_access_key'],region: 'us-east-1')
+
   end
 
   def test()
 
-      @sesClient.get_send_quota.each do |response|
-        @resp = response.data.max_send_rate
-      end
+      # @sesClient.get_send_quota.each do |response|
+      #   @resp = response.data.max_send_rate
+      # end
+
+
+
+      # @sesClient.list_verified_email_addresses.each do |response|
+      #   @resp = response.data
+      # end
+
+      email = ''
+
+      @sesClient.verify_email_address(email_address: email)
 
   end
 end
