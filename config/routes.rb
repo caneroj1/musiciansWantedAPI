fece96964043ff02d6bfe1fbe46b34ec8136b25e
caneroj1 <caneroj1@tcnj.edu>
2015-03-13 14:18:38 -0400

    added a welcome view controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-  root "api/v1/users#index"
+  root "welcome#index"
 
   namespace :api, defaults: { format: :json } do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
