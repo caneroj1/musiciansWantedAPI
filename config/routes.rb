df4ebeaaa27d5e677deecd67afa98ce7af9426f6
Hank Harvey <harveyh1@tcnj.edu>
2015-03-13 18:21:37 -0400

    Fixed routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 require 'api_constraints'
 
 Rails.application.routes.draw do
-
-  namespace :api do
-
   root "welcome#index"
 
   namespace :api, defaults: { format: :json } do
-    master
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :users, defaults: { format: :json }
       get 'test', to: 'ses_emails#test'
