4fa20f81af7df2856600fa4393ab2e60468fd3bc
Hank Harvey <harveyh1@tcnj.edu>
2015-03-17 11:46:41 -0400

    dotenv was removed from gemfile, added back. still can't display env variables

--- app/controllers/api/v1/ses_emails_controller.rb
+++ app/controllers/api/v1/ses_emails_controller.rb
@@ -4,7 +4,12 @@ class Api::V1::SesEmailsController < ApplicationController
 
   # Load AWS SES Client
   def loadSESClient()
-    @sesClient = Aws::SES::Client.new(access_key_id: ENV['aws_access_key_id'],secret_access_key: ENV['aws_secret_access_key'],region: 'us-east-1')
+    @sesClient = Aws::SES::Client.new(access_key_id: ENV['SES_aws_access_key_id'],secret_access_key: ENV['SES_aws_secret_access_key'],region: 'us-east-1')
+    puts "Your environment variable should print out below"
+    environment = ENV['S3_aws_access_key_id']
+    myVar = "==========  #{environment}  =========="
+
+    puts myVar
 
   end
 
@@ -22,6 +27,8 @@ class Api::V1::SesEmailsController < ApplicationController
 
       email = 'testCSC470@mailinator.com'
 
+
+      puts "Have I broken yet"
       @sesClient.verify_email_address(email_address: email)
 
       @resp = "Verification Email Sent"
