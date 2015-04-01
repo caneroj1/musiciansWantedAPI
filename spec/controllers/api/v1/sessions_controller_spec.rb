f20b0f93d64876735039a428721db80e60076ccf
caneroj1 <caneroj1@tcnj.edu>
2015-03-31 23:07:25 -0400

    made tests pass.

--- spec/controllers/api/v1/sessions_controller_spec.rb
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe Api::V1::SessionsController, type: :controller do
       before(:each) do
         post :login, { username: ENV["soundcloud_username"],
                        password: ENV["soundcloud_password"] }, format: :json
-        login_response = json_response
+        @login_response = json_response
       end
 
       it 'will return json for soundcloud' do
-        expect(login_response).to have_key(:soundcloud)
+        expect(@login_response).to have_key(:refresh_token)
       end
 
       it 'will return the user id for the correct user' do
-        expect(login_response).to have_key(:user_id)
+        expect(@login_response).to have_key(:user_id)
       end
     end
   end
