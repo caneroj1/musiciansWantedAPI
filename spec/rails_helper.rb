e0ffb290eb6485fb731894bef0b00909b1b68395
caneroj1 <caneroj1@tcnj.edu>
2015-04-06 16:19:04 -0400

    unbroke the tests.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,20 +54,4 @@ RSpec.configure do |config|
   config.before(:each, type: :controller) do
     include_default_accept_headers
   end
-
-  Geocoder.configure(:lookup => :test)
-
-  Geocoder::Lookup::Test.add_stub(
-    "New York, NY", [
-      {
-        'latitude'     => 40.7143528,
-        'longitude'    => -74.0059731,
-        'address'      => 'New York, NY, USA',
-        'state'        => 'New York',
-        'state_code'   => 'NY',
-        'country'      => 'United States',
-        'country_code' => 'US'
-      }
-    ]
-  )
 end
